8c52b6d92a191611925b2eea769a8c766d019fc6
Nicole Jackson <nicolejackson8990@gmail.com>
2024-04-13 09:22:42 +0000

    Preps code for deploy

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: gunicorn recipeshare.wsgi
+
+
--- recipeshare/settings.py
+++ recipeshare/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#o%q*)$k&5g5+r00+-n14y4*rl3@bhdgsf$g6=n=!e=o-@ig@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-nicolejacks-pp4recipesh-3piobwtroln.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-nicolejacks-pp4recipesh-3piobwtroln.ws-eu110.gitpod.io',
+                 '.herokuapp.com']
 
 
 # Application definition
