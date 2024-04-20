a20ef75eee5cb78ddb03880db309107ef223f528
Nicole Jackson <nicolejackson8990@gmail.com>
2024-04-20 12:25:24 +0000

    Add: Mock-up recipes

--- blog/admin.py
+++ blog/admin.py
@@ -8,7 +8,7 @@ class PostAdmin(SummernoteModelAdmin):
     """
     Lists fields for display in admin, search & filter fields.
     """
-    list_display = ('title', 'author', 'status',)
+    list_display = ('title', 'author', 'status','created_on',)
     search_fields = ['title', 'ingredients',]
     list_filter = ('status', 'created_on',)
     # prepopulated_fields = {'slug': ('title',)}
