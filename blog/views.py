be909d83240e775a92fb2cec2f0ebda52540a213
Nicole Jackson <nicolejackson8990@gmail.com>
2024-04-24 19:54:19 +0000

    Add: Basic comment displaying html

--- blog/views.py
+++ blog/views.py
@@ -17,15 +17,35 @@ class RecipeList(generic.ListView):
 def recipe_detail(request, slug):
     """
     Displays an individual recipe post :model:`blog.Recipe`.
+    **Context**
+
+    ``recipe``
+        An instance of :model:`blog.Recipe`.
+    ``comments``
+        All approved comments related to the recipe post.
+    ``total_comments``
+        A total of approved comments related to the recipe post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
+
+    **Template:**
+
+    :template:`blog/recipe_detail.html`
     """
 
     queryset = Recipe.objects.filter(status=1)
     recipe = get_object_or_404(queryset, slug=slug)
+    comments = recipe.comments.all().order_by("-created_on")
+    total_comments = recipe.comments.filter(approved=True).count()
 
     return render(
         request,
         "blog/recipe_detail.html",
-        {"recipe": recipe},
+        {
+            "recipe": recipe,
+            "comments": comments,
+            "total_comments": total_comments,
+        }
         # {"recipe": recipe,
         #  "coder": "Matt Rudge"},
     )
