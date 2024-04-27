e368a69f8c70d6aaa84d86a9a80264704c134d05
Nicole Jackson <nicolejackson8990@gmail.com>
2024-04-27 19:15:22 +0000

    Add: Contact us model to the about app

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About
+from .models import About, ContactUs
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -9,3 +9,10 @@ class AboutAdmin(SummernoteModelAdmin):
     Adds text editing of the body in admin panel
     """
     summernote_fields = ('body',)
+
+
+# WILL NEED TO GO TO contact us page
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
